10ee174a428f1e8866db0c91eb5b78ea4125ec21
Louis Williams <louis.williams@mongodb.com>
2018-03-26 14:02:18 -0400

    Improve tests

--- jstests/replsets/secondary_reads_unique_indexes.js
+++ jstests/replsets/secondary_reads_unique_indexes.js
@@ -45,7 +45,7 @@
     assert.commandWorked(testDB.runCommand(
         {createIndexes: testCollName, indexes: [{key: {x: 1}, name: "x_1", unique: true}]}));
 
-    // We want to do applyOps with at last as many different documents as there are parallel batch
+    // We want to do updates with at least as many different documents as there are parallel batch
     // writer threads (16). Each iteration increments and decrements a uniquely indexed value, 'x'.
     // The goal is that a reader on a secondary might find a case where the unique index constraint
     // is ignored, and an index on x maps to two different records.
@@ -54,14 +54,20 @@
     const nReaders = 16;
 
     // Do a bunch of reads using the 'x' index on the secondary.
-    let readCmd = "db.getMongo().setSlaveOk();" + "for (let i = 0; i < " + nIterations +
-        "; i++) { for (let x = 0; x < " + nOps + "; x++) {" + "assert.commandWorked(" +
-        "db.getSiblingDB('test').runCommand({find: '" + testCollName +
-        "', filter: {x: x}, projection: {x: 1}}));}}";
-    print("Read cmd: " + readCmd);
+    // No errors should be encountered on the secondary.
     let readers = [];
+    let readCmd = `
+        db.getMongo().setSlaveOk();
+        while (true) {
+            for (let x = 0; x < ${nOps}; x++) {
+                assert.commandWorked(db.getSiblingDB('test').runCommand(
+                    {find: "${testCollName}", filter: {x: x}, projection: {x: 1}}));
+            }
+        }`;
+
     for (let i = 0; i < nReaders; i++) {
-        readers[i] = startParallelShell(readCmd, secondary.port);
+        readers[i] =
+            startMongoProgramNoConnect("mongo", "--port", secondary.port, "--eval", readCmd);
         print("reader " + i + " started");
     }
 
@@ -76,31 +82,32 @@
 
     // Cycle the value of x in the document {_id: i, x: i} between i and i+1 each iteration.
     for (let iteration = 0; iteration < nIterations; iteration++) {
-        let ops = [];
+        let updates = [];
         // Reset each document.
         for (let i = 0; i < nOps; i++) {
-            ops[i] = {op: "u", ns: testNs, o2: {_id: i}, o: {x: i, iter: iteration}};
+            updates[i] = {q: {_id: i}, u: {x: i, iter: iteration}};
         }
-        assert.commandWorked(testDB.runCommand({applyOps: ops}));
-        ops = [];
+        assert.commandWorked(testDB.runCommand({update: testCollName, updates: updates}));
+        updates = [];
 
-        // Generate an applyOps command that increments x on each document backwards by _id to avoid
-        // conficts when applied in-order. When these updates get applied to the secondary, they may
-        // get applied out of order by different threads and temporarily violate unique index
+        // Generate updates that increment x on each document backwards by _id to avoid conficts
+        // when applied in-order. When these updates get applied to the secondary, they may get
+        // applied out of order by different threads and temporarily violate unique index
         // constraints.
         for (let i = 0; i < nOps; i++) {
             // Start at the end and increment x by 1.
             let end = nOps - i - 1;
             let nextX = end + 1;
-            ops[i] = {op: "u", ns: testNs, o2: {_id: end}, o: {x: nextX, iter: iteration}};
+            updates[i] = {q: {_id: end}, u: {x: nextX, iter: iteration}};
         }
         print('iteration ' + iteration);
-        assert.commandWorked(testDB.runCommand({applyOps: ops}));
+        assert.commandWorked(testDB.runCommand({update: testCollName, updates: updates}));
     }
 
     for (let i = 0; i < nReaders; i++) {
-        let readerWait = readers[i];
-        readerWait();
+        const ec = stopMongoProgramByPid(readers[i]);
+        const expect = _isWindows() ? 1 : -15;
+        assert.eq(ec, expect, "Expected mongo shell to exit with code 0");
         print("reader " + i + " done");
     }
     rst.stopSet();

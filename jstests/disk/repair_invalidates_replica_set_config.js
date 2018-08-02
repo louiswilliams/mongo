08a768b2eafc4779fca37f2f0a52c677bc8ed4d0
Louis Williams <louis.williams@mongodb.com>
2018-08-02 17:30:46 -0400

    jstest that secondary cannot rejoin replica set after repair modifies data

---
+++ jstests/disk/repair_invalidates_replica_set_config.js
@@ -0,0 +1,82 @@
+/**
+ * This test corrupts WiredTiger data files on a secondary, repairs the data, and asserts that the
+ * node is unable to re-join its original replica set without an initial sync.
+ *
+ * @tags: [requires_wiredtiger]
+ */
+
+(function() {
+
+    load('jstests/disk/libs/wt_file_helper.js');
+
+    const baseName = "wt_repair_missing_files";
+    const dbName = "repair_missing_files";
+    const collName = "test";
+
+    let replSet = new ReplSetTest({nodes: 2});
+    replSet.startSet();
+    replSet.initiate();
+    replSet.awaitReplication();
+
+    let primary = replSet.getPrimary();
+    const originalSecondary = replSet.getSecondary();
+    let secondary = originalSecondary;
+
+    let primaryDB = primary.getDB(dbName);
+    let secondaryDB = secondary.getDB(dbName);
+
+    assert.writeOK(primaryDB[collName].insert({_id: 0, foo: "bar"}));
+    replSet.awaitLastOpCommitted();
+
+    const secondaryPort = secondary.port;
+    const secondaryDbpath = secondary.dbpath;
+    let secondaryCollUri = getUriForColl(secondaryDB[collName]);
+    let secondaryCollFile = secondaryDbpath + "/" + secondaryCollUri + ".wt";
+
+    // Shut down a secondary. Delete the collection's data file.
+    MongoRunner.stopMongod(secondary);
+    jsTestLog("Deleting secondary collection file: " + secondaryCollFile);
+    removeFile(secondaryCollFile);
+
+    // Ensure the secondary can be repaired successfully.
+    jsTestLog("Repairing the secondary");
+    assertRepairSucceeds(secondaryPort, secondaryDbpath);
+
+    // Starting up with --replSet should fail with a specific error.
+    jsTestLog("The repaired secondary should fail to start up with the --replSet option");
+    clearRawMongoProgramOutput();
+    secondary = MongoRunner.runMongod({
+        dbpath: secondaryDbpath,
+        port: secondaryPort,
+        replSet: replSet.getReplSetConfig()._id,
+        noCleanData: true,
+        waitForConnect: false
+    });
+    assert.soon(function() {
+        return rawMongoProgramOutput().indexOf("Fatal Assertion 50895") >= 0;
+    });
+    MongoRunner.stopMongod(secondary, null, {allowedExitCode: MongoRunner.EXIT_ABRUPT});
+
+    // Starting up without --replSet should not fail, but the collection should exist with no data.
+    jsTestLog(
+        "The repaired secondary should start up and serve reads without the --replSet option");
+    secondary =
+        MongoRunner.runMongod({dbpath: secondaryDbpath, port: secondaryPort, noCleanData: true});
+    assert.neq(null, secondary);
+    secondaryDB = secondary.getDB(dbName);
+    assert(secondaryDB[collName].exists());
+    assert.eq(secondaryDB[collName].find().itcount(), 0);
+    MongoRunner.stopMongod(secondary);
+
+    // Starting the secondary with a wiped data directory should force an initial sync.
+    jsTestLog("The secondary with a wiped data directory should resync successfully");
+    secondary = replSet.start(originalSecondary, {dbpath: secondaryDbpath, port: secondaryPort});
+    secondaryDB = secondary.getDB(dbName);
+
+    // Wait for the secondary to catch up.
+    replSet.awaitSecondaryNodes();
+    assert.eq(secondaryDB[collName].find().itcount(), 1);
+
+    replSet.stopSet();
+
+})();
